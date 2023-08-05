a83ac3eb9f69df2d573120e1810f09312ea10945
Drumond <drumond.work@gmail.com>
2023-08-05 01:36:31 -0300

    TESTE

--- app.js
+++ app.js
@@ -20,12 +20,6 @@ const connection = mysql.createConnection({
 //INICIALIZATION CONNECTION WITH DATABASE
 connection.connect()
 
-//CONNECTION TEST
-connection.query("SELECT 1+1 AS solution", (err, rows, fields) => {
-    if (err) throw err;
-
-    console.log("The solution is: ", rows[0].solution)
-});
 
 app.use(cors({ origin: "*", }));
 
