70e05ffe7e4582408ccededa641c5f0afcb776c6
Drumond <drumond.work@gmail.com>
2023-08-05 02:13:10 -0300

    dasdadadddd

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const mysql = require("mysql")
 const cors = require("cors")
 
 const app = express()
-const PORT = process.env.PORT || 3030;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
